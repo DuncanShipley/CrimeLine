a955481debf1b94cda6bea948b7cee830a71b739
AzureAlexis <alex.hamilton2025@gmail.com>
2023-09-28 10:44:43 -0700

    Basic tilemap

--- Assets/Scenes/Framework/Alexis WIP/camfollow.cs
+++ Assets/Scenes/Framework/Alexis WIP/camfollow.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CamFollow : MonoBehaviour
+public class camfollow : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,7 @@ public class CamFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = GameObject.Find("Player").transform.position + new Vector3(0, 1, -5);
     }
+
 }
