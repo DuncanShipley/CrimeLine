b7a9a0e90607371c300c307d790045d8382e0995
AzureAlexis <alex.hamilton2025@gmail.com>
2023-10-04 11:44:04 -0700

    LOCKED DOORSSSS

---
+++ Assets/Scenes/PuzzleStealth/alexisworking/keycode.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keycode : MonoBehaviour
+{
+
+    // Colider
+    private void start()
+    {
+        GameObject.Find("keycard").GetComponent<Canvas>().enabled = false;
+        Debug.Log(GameObject.Find("keycard").GetComponent<Canvas>().enabled);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag=="Player"){
+            keys heldKeysScript = GameObject.Find("Player").GetComponent<keys>();
+            heldKeysScript.addKey();
+            GameObject.Find("keycard").GetComponent<Canvas>().enabled = true;
+            Destroy(gameObject);
+        }
+    }
+}
