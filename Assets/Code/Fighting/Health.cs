62106de1919374429ef0c2fd9b3fc838deb1c812
Casey <caseyluttkus@gmail.com>
2023-10-10 10:50:47 -0700

    Feat(fighting): Add more animations and start on attacking

--- Assets/Code/Fighting/Health.cs
+++ Assets/Code/Fighting/Health.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AttackScript : MonoBehaviour
+public class Health : MonoBehaviour
 {
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +14,5 @@ public class AttackScript : MonoBehaviour
     void Update()
     {
         
-    }
-
-    void Punch()
-    {
-
     }
 }
