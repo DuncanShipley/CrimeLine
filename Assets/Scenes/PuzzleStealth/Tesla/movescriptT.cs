9283f9b2988212e33984c847060120d7ab9bc0f8
Tesla <teslachin@gmail.com>
2023-10-20 10:38:48 -0700

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scenes/PuzzleStealth/Tesla/movescriptT.cs
+++ Assets/Scenes/PuzzleStealth/Tesla/movescriptT.cs
@@ -10,6 +10,7 @@ public class movescriptT : MonoBehaviour
     float h = 0f;
     float v = 0f;
     float throwPause = 0;
+    int thrown = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -44,11 +45,12 @@ public class movescriptT : MonoBehaviour
                 lastInput.y = 0;
         }
         throwPause += Time.deltaTime;
-        Debug.Log(throwPause);
 
         //creates a projectile moving in the same direction as last player movement
         if (Input.GetKeyDown(KeyCode.E) && projectileT.availible > 0 && throwPause>=1)
         {
+            thrown++;
+            Debug.Log(thrown);
             throwPause = 0;
             //projectileT.availible--;
             copy = Instantiate(projectile, gameObject.transform.position, new Quaternion());
