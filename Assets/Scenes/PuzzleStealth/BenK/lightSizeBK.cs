72d7a42b3d72b0d060013ad3c5884646a7b8a007
Ben1 <bkunze9660@student.pps.net>
2023-12-12 10:11:27 -0800

    merging changes with main

--- Assets/Scenes/PuzzleStealth/BenK/lightSizeBK.cs
+++ Assets/Scenes/PuzzleStealth/BenK/lightSizeBK.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class lightSizeBK : MonoBehaviour
 {
     Color passiveColor;
+    private float oldInnerAngle;
+    private float oldOuterAngle;
+    private float oldOuterRadius;
     UnityEngine.Rendering.Universal.Light2D myLight;
     void Start()
     {
@@ -15,10 +18,26 @@ public class lightSizeBK : MonoBehaviour
 
     void Update()
     {
-        myLight.pointLightInnerAngle = WaypointFollowerBK.suspicious*45 + 75;
-        myLight.pointLightOuterAngle = WaypointFollowerBK.suspicious*45 + 75;
-        myLight.pointLightOuterRadius = WaypointFollowerBK.suspicious*2 + 9;
-        myLight.color = Color.Lerp(passiveColor, Color.red, WaypointFollowerBK.suspicious);
-        // these change the light's radius, angle, and color based on the guard's suspicion.
+        if (guardHealthBK.alive && !guardHealthBK.dying)
+        {
+            myLight.pointLightInnerAngle = WaypointFollowerBK.suspicious * 45 + 75;
+            myLight.pointLightOuterAngle = WaypointFollowerBK.suspicious * 45 + 75;
+            myLight.pointLightOuterRadius = WaypointFollowerBK.suspicious * 2 + 9;
+            myLight.color = Color.Lerp(passiveColor, Color.red, WaypointFollowerBK.suspicious);
+            // these change the light's radius, angle, and color based on the guard's suspicion.
+            
+            oldInnerAngle = myLight.pointLightInnerAngle;
+            oldOuterAngle = myLight.pointLightOuterAngle;
+            oldOuterRadius = myLight.pointLightOuterRadius;
+            //saves the necessary light values for when the guard dies.
+        }
+        if (guardHealthBK.dying)
+        {
+            myLight.pointLightInnerAngle = myLight.pointLightInnerAngle - oldInnerAngle * (Time.deltaTime / 2);
+            myLight.pointLightOuterAngle = myLight.pointLightOuterAngle - oldOuterAngle * (Time.deltaTime / 2);
+            myLight.pointLightOuterRadius = myLight.pointLightOuterRadius - oldOuterRadius * (Time.deltaTime / 2);
+            myLight.intensity = myLight.intensity - 1.5f * Time.deltaTime;
+
+        }
     }
 }
