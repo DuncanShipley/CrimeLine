abf1bcd209befbbb05c3082b3cafa3dabdab1736
rotating.cow <ben.arkonovich@gmail.com>
2023-12-12 11:12:49 -0800

    feat(AI) set up AI framework, unimplemented

---
+++ Assets/Code/Fighting/AIs/KenAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+using Assets.Code.Fighting;
+
+namespace Assets.Code.Fighting.AIs
+{
+    class KenAI : EnemyAIAction
+    {
+
+        public override string Output(EnemyAiInput input)
+        {
+            return "pluh";
+        }
+
+    }
+}
