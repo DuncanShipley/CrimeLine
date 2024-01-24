6224d73b378c6956100f1c2837b4437641efe8ed
Jonah <jzakreski4879@student.pps.net>
2024-01-24 10:07:53 -0800

    (Dating Sim) Stops unity form crashing, shows one line at once

--- Assets/Code/Dating/Ink Manager.cs
+++ Assets/Code/Dating/Ink Manager.cs
@@ -20,10 +20,15 @@ public class InkManager : MonoBehaviour
 
     private void Update()
     {
-        // if (story.currentChoices ==0)
-        if (Input.GetMouseButtonDown(0))
-        {
-            RefreshView();
+        // Displays a new line every time you click unless there is a choice 
+        if (story.canContinue == true) { 
+            if (story.currentChoices.Count == 0)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RefreshView();
+                }
+            }
         }
     }
 
@@ -31,62 +36,74 @@ public class InkManager : MonoBehaviour
     void StartStory()
     {
         story = new Story(inkJSONAsset.text);
-        story.BindExternalFunction("place_actors", (string leftName, string rightName) =>
+        if (OnCreateStory != null) OnCreateStory(story);
+            story.BindExternalFunction("place_actors", (string leftName, string rightName) =>
         {
             cm.PlaceActors(leftName, rightName);
         });
         story.BindExternalFunction("change_emotion", (string emotion, int ID) =>
         {
             cm.ChangeActorEmotion(emotion, ID);
-        });
+        }); 
+
         RefreshView();
     }
 
     // This is the main function called every time the story changes. It does a few things:
     // Destroys all the old content and choices.
     // Continues over all the lines of text, then displays all the choices. If there are no choices, the story is finished!
+    public static int LD = 1;
     void RefreshView()
     {
         // Remove all the UI on screen
         RemoveChildren();
 
-        // Read all the content until we can't continue any more
-        int r = 1;
-        while (r != 0)
+        //Displays one line of text at a time
+        LD = 1;
+        while (LD != 0)
         {
-            //bool next = .canContinue();
-            // Continue gets the next line of the story
-            string text = story.Continue();
-            // This removes any white space from the text.
-            text = text.Trim();
-            // Display the text on screen!
-            CreateContentView(text);
-            r--;
+           
+            if (story.canContinue==true) { 
+               // Continue gets the next line of the story
+                string text = story.Continue();
+                // This removes any white space from the text.
+                text = text.Trim();
+                // Display the text on screen!
+                CreateContentView(text);
+                LD --;
+            }
         }
 
         // Display all the choices, if there are any!
         if (story.currentChoices.Count > 0)
         {
+            BP = 0; 
+            TR = 0;
             for (int i = 0; i < story.currentChoices.Count; i++)
             {
                 Choice choice = story.currentChoices[i];
                 Button button = CreateChoiceView(choice.text.Trim());
                 // Tell the button what to do when we press it
-                button.onClick.AddListener(delegate
-                {
+                button.onClick.AddListener(delegate {
                     OnClickChoiceButton(choice);
                 });
             }
         }
+        if (story.canContinue==false)
+        {
+            if(story.currentChoices.Count == 0)
+            {
+             print("No more choices :(");
+            }
+        }            
     }
 
 
     // When we click the choice button, tell the story to choose that choice!
-    void OnClickChoiceButton(Choice choice)
-    {
-        story.ChooseChoiceIndex(choice.index);
-        RefreshView();
-    }
+    void OnClickChoiceButton (Choice choice) {
+		story.ChooseChoiceIndex (choice.index);
+		RefreshView();
+	}
 
     // Creates a textbox showing the the line of text
     void CreateContentView(string text)
@@ -98,42 +115,36 @@ public class InkManager : MonoBehaviour
 
     }
 
-    public static int t = 0;
+    
     void CreateContentView(string text, Transform transform)
     {
-        if (t == 0)
-        {
         Text storyText = Instantiate(textPrefab) as Text;
         storyText.text = text;
-        //storyText.transform.SetParent(panel.transform, false);
-        t++;
-        }
-        else
-        {
-            Text storyText = Instantiate(textPrefab, new Vector3(0, -150.0f, 0), Quaternion.identity) as Text;
-            storyText.text = text;
-            t--;
-        }
-
     }
 
-    // Creates a button showing the choice text
+    //moves the button down 35 units on the y axis
+    public static int BP = 0;
+    //the amount of buttons being created 
+    public static int TR = 0;
+    // Creates a button showing the choice 
     Button CreateChoiceView(string text)
     {
-        // Creates the button from a prefab
-        Button choice = Instantiate(buttonPrefab) as Button;
-        choice.transform.SetParent(canvas.transform, false);
+     // Creates the button from a prefab
+     Button choice = Instantiate(buttonPrefab, new Vector3(0, -80.0f-(BP*TR), 0), Quaternion.identity) as Button;
+     choice.transform.SetParent(canvas.transform, false);
 
-        // Gets the text from the button prefab
-        Text choiceText = choice.GetComponentInChildren<Text>();
-        choiceText.text = text;
+     // Gets the text from the button prefab
+     Text choiceText = choice.GetComponentInChildren<Text>();
+     choiceText.text = text;
 
-        // Make the button expand to fit the text
-        HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
-        layoutGroup.childForceExpandHeight = false;
+     // Make the button expand to fit the text
+     HorizontalLayoutGroup layoutGroup = choice.GetComponent<HorizontalLayoutGroup>();
+     layoutGroup.childForceExpandHeight = false;
+     BP=35;
+     TR++;
 
-        return choice;
-    }
+     return choice;
+     }
 
     // Destroys all the children of this gameobject (all the UI)
     void RemoveChildren()
