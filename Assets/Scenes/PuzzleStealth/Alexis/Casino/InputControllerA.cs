f90cfc7ccbe6386c34b99c6cb4d576de0df3b762
AzureAlexis <alex.hamilton2025@gmail.com>
2024-02-21 11:20:37 -0800

    Put most up-to-date version in share + created basis for new level

---
+++ Assets/Scenes/PuzzleStealth/Alexis/Casino/InputControllerA.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputControllerA : MonoBehaviour
+{
+    bool zReady = true;
+    bool zTrigger = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Z) && zReady)
+        {
+            zTrigger = true;
+            zReady = false;
+        } 
+        else if(!Input.GetKey(KeyCode.Z))
+        {
+            zTrigger = false;
+            zReady = true;
+        } else
+        {
+            zTrigger = false;
+            zReady = false;
+        }
+    }
+
+    public bool GetKeyLimited(string key)
+    {
+        switch (key) 
+        {
+            case "z":
+                return zTrigger;
+            default:
+                return false;
+        }
+        
+    }
+}
---
+++ Assets/Scenes/PuzzleStealth/Alexis/Casino/lightSizeA.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightSizeA : MonoBehaviour
+{
+    Color passiveColor;
+    UnityEngine.Rendering.Universal.Light2D myLight;
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLight = GetComponent<UnityEngine.Rendering.Universal.Light2D>();
+        passiveColor = myLight.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        id = gameObject.transform.parent.parent.GetComponent<IDs>().GetID();
+
+        myLight.pointLightInnerAngle = WaypointFollower.sus[id] * 45 + 75;
+        myLight.pointLightOuterAngle = WaypointFollower.sus[id] * 45 + 75;
+        myLight.pointLightOuterRadius = WaypointFollower.sus[id] * 2 + 9;
+        myLight.color = Color.Lerp(passiveColor, Color.red, WaypointFollower.sus[id]);
+        // these change the light's radius, angle, and color based on the guard's suspicion.
+    }
+}
