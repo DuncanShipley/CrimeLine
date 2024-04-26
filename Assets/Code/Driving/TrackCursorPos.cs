8960003da43b561ec617ff70fd978fdba6e2a3fb
Ari <agrace8513@student.pps.net>
2024-04-26 11:41:03 -0700

    https://trello.com/c/qT6Sc3hF/52-animate-menu

---
+++ Assets/Code/Driving/TrackCursorPos.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackCursorPos : MonoBehaviour
+{
+    public GameObject[] buttons;
+
+    public Animator carSpin;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        Vector3 mousePos = Input.mousePosition;
+
+        for(int i = 0; i < buttons.Length; i++)
+        {
+            float x = buttons[i].transform.position.x;
+            float y = buttons[i].transform.position.y;
+            float height = buttons[i].GetComponent<RectTransform>().rect.height;
+            float length = buttons[i].GetComponent<RectTransform>().rect.width;
+
+            float cx = mousePos.x;
+            float cy = mousePos.y;
+
+            if((cx >= x && cx <= x + length) && (cy >= y && cy <= y + height))
+            {
+                Debug.Log("Cursor Found");
+                carSpin.SetTrigger("ButtonHighlight");
+            }
+        }
+    }
+}
