b971f130a350cbabd7ae2e3b351d643d9952452d
Duncan S <dshipley4758@student.pps.net>
2024-05-22 11:08:36 -0700

    made scene for testing random spawn points
    https://trello.com/c/hN1PE9am

--- Assets/Code/Driving/Random Spawn Point.cs
+++ Assets/Code/Driving/Random Spawn Point.cs
@@ -13,17 +13,17 @@ public class RandomSpawnPoint : MonoBehaviour
         print("spawn randnum " + randNum);
 
         if(randNum <= 33){
-            Vector3 spawnPosition = new Vector3(647,0,425)
+            Vector3 spawnPosition = new Vector3(647,0,425);
             
         }
         else if(randNum <=66){
-            Vector3 spawnPosition = new Vector3(647,0,425)
+            Vector3 spawnPosition = new Vector3(647,0,425);
         }
         else if(randNum <=100){
-            Vector3 spawnPosition = new Vector3(647,0,425)
+            Vector3 spawnPosition = new Vector3(647,0,425);
         }
 
-        Instantiate(car, spawnPosition, )
+        //Instantiate(car, spawnPosition, );
     }
 
 }
